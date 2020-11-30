26dca7756ed6405441590771694167c2a59c6d34
Nonentityboy <1138580559@qq.com>
2020-11-30 21:44:04 +0800

    h h g

--- js/moveMoon.js
+++ js/moveMoon.js
@@ -1,11 +1,6 @@
+// 获得盒子最大移动位置
 const body = document.querySelector('body');
 const div = document.getElementById('footer-bottom-middle');
-
-console.log(body)
-console.log(body.offsetHeight);
-console.log(div)
-console.log(div.offsetHeight)
-    // 获得盒子最大移动位置
 const maxTop = body.offsetHeight - div.offsetHeight;
 // 获取手指初始坐标 
 var startY = 0;
@@ -25,16 +20,21 @@ div.addEventListener('touchend', function(e) {
 });
 // 手指按住移动 
 div.addEventListener('touchmove', function(e) {
+    const body = document.querySelector('body');
+    const div = document.getElementById('footer-bottom-middle');
+
+    const maxTop = body.offsetHeight - div.offsetHeight;
     // 计算手指的移动距离：手指移动之后的坐标减去手指初始的坐标 
     let moveY = e.targetTouches[0].pageY - startY;
     let lastTop = y + moveY;
     //防止超出父元素范围
-    if (lastTop < 0) lastTop = 0;
-    if (lastTop > maxTop) lastTop = maxTop;
+    if (lastTop <= 0) lastTop = 0;
+    if (lastTop >= maxTop) lastTop = maxTop;
     // 移动盒子 盒子原来的位置 + 手指移动的距离 
     this.style.top = lastTop + 'px';
     // 控制台 打印位置
-    console.log(lastTop + 'px');
+    console.log('顶部最大高度' + maxTop)
+    console.log('距离顶部高度' + lastTop + 'px');
     // 阻止屏幕滚动的默认行为 
     e.preventDefault();
 });
