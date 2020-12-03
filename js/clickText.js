2c3205be365b52a16e7cbc12b234faf86371964b
Nonentityboy <1138580559@qq.com>
2020-12-03 19:52:17 +0800

    update

--- js/clickText.js
+++ js/clickText.js
@@ -1,10 +1,13 @@
 //获取点击事件的元素
 const shenren = document.getElementById('shenren');
 
-const btn = document.getElementsByClassName('btn');
-
 //添加事件监听函数
-shenren.addEventListener('touchstart', function () {
-  console.log("点击神人！");
-  btn.style.zIndex = 15;
+shenren.addEventListener('touchstart', function() {
+    const btn = document.getElementById('shenrenClick');
+    btn.style.zIndex = 15;
+})
+
+shenren.addEventListener('touchend', function() {
+    const btn = document.getElementById('shenrenClick');
+    btn.style.zIndex = -15;
 })
