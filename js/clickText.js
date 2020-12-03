59c67dab68bd76a1d922783ff168653eb13925d1
Nonentityboy <1138580559@qq.com>
2020-12-04 01:55:03 +0800

    update clicktext music

--- js/clickText.js
+++ js/clickText.js
@@ -8,90 +8,93 @@ const yueer = document.getElementById('yueer');
 const xiaoxiang = document.getElementById('xiaoxiang');
 
 //添加事件监听函数
-shenren.addEventListener('touchstart', function () {
-  const btn = document.getElementById('shenrenClick');
-  shenren.style.opacity = 0;
-  btn.style.opacity = 1;
+shenren.addEventListener('touchstart', function() {
+    const btn = document.getElementById('shenrenClick');
+    shenren.style.opacity = 0;
+    btn.style.opacity = 1;
+    var audio = document.getElementById('shenrenchang');
+    audio.play(); //audio.play();// 这个就是播放  
 })
 
-shenren.addEventListener('touchend', function () {
-  const btn = document.getElementById('shenrenClick');
-  shenren.style.opacity = 1;
-  btn.style.opacity = 0;
+shenren.addEventListener('touchend', function() {
+    const btn = document.getElementById('shenrenClick');
+    shenren.style.opacity = 1;
+    btn.style.opacity = 0;
+    var audio = document.getElementById('shenrenchang');
+    audio.pause();
 })
 
 //妆台秋思
-zhuangtai.addEventListener('touchstart', function () {
-  const btn = document.getElementById('zhuangtaiClick');
-  zhuangtai.style.opacity = 0;
-  btn.style.opacity = 1;
+zhuangtai.addEventListener('touchstart', function() {
+    const btn = document.getElementById('zhuangtaiClick');
+    zhuangtai.style.opacity = 0;
+    btn.style.opacity = 1;
 })
 
-zhuangtai.addEventListener('touchend', function () {
-  const btn = document.getElementById('zhuangtaiClick');
-  zhuangtai.style.opacity = 1;
-  btn.style.opacity = 0;
+zhuangtai.addEventListener('touchend', function() {
+    const btn = document.getElementById('zhuangtaiClick');
+    zhuangtai.style.opacity = 1;
+    btn.style.opacity = 0;
 })
 
 //梅花
-meihua.addEventListener('touchstart', function () {
-  const btn = document.getElementById('meihuaClick');
-  meihua.style.opacity = 0;
-  btn.style.opacity = 1;
+meihua.addEventListener('touchstart', function() {
+    const btn = document.getElementById('meihuaClick');
+    meihua.style.opacity = 0;
+    btn.style.opacity = 1;
 })
 
-meihua.addEventListener('touchend', function () {
-  const btn = document.getElementById('meihuaClick');
-  meihua.style.opacity = 1;
-  btn.style.opacity = 0;
-})
-//广陵
-guangling.addEventListener('touchstart', function () {
-  const btn = document.getElementById('guanglingClick');
-  guangling.style.opacity = 0;
-  btn.style.opacity = 1;
-})
-
-guangling.addEventListener('touchend', function () {
-  const btn = document.getElementById('guanglingClick');
-  guangling.style.opacity = 1;
-  btn.style.opacity = 0;
-})
-//霓裳
-nichang.addEventListener('touchstart', function () {
-  const btn = document.getElementById('nichangClick');
-  nichang.style.opacity = 0;
-  btn.style.opacity = 1;
+meihua.addEventListener('touchend', function() {
+        const btn = document.getElementById('meihuaClick');
+        meihua.style.opacity = 1;
+        btn.style.opacity = 0;
+    })
+    //广陵
+guangling.addEventListener('touchstart', function() {
+    const btn = document.getElementById('guanglingClick');
+    guangling.style.opacity = 0;
+    btn.style.opacity = 1;
 })
 
-nichang.addEventListener('touchend', function () {
-  const btn = document.getElementById('nichangClick');
-  nichang.style.opacity = 1;
-  btn.style.opacity = 0;
+guangling.addEventListener('touchend', function() {
+        const btn = document.getElementById('guanglingClick');
+        guangling.style.opacity = 1;
+        btn.style.opacity = 0;
+    })
+    //霓裳
+nichang.addEventListener('touchstart', function() {
+    const btn = document.getElementById('nichangClick');
+    nichang.style.opacity = 0;
+    btn.style.opacity = 1;
+})
+
+nichang.addEventListener('touchend', function() {
+    const btn = document.getElementById('nichangClick');
+    nichang.style.opacity = 1;
+    btn.style.opacity = 0;
 })
 
 //月儿
-yueer.addEventListener('touchstart', function () {
-  const btn = document.getElementById('yueerClick');
-  yueer.style.opacity = 0;
-  btn.style.opacity = 1;
+yueer.addEventListener('touchstart', function() {
+    const btn = document.getElementById('yueerClick');
+    yueer.style.opacity = 0;
+    btn.style.opacity = 1;
 })
 
-yueer.addEventListener('touchend', function () {
-  const btn = document.getElementById('yueerClick');
-  yueer.style.opacity = 1;
-  btn.style.opacity = 0;
-})
-// 潇湘
-xiaoxiang.addEventListener('touchstart', function () {
-  const btn = document.getElementById('xiaoxiangClick');
-  xiaoxiang.style.opacity = 0;
-  btn.style.opacity = 1;
-})
-
-xiaoxiang.addEventListener('touchend', function () {
-  const btn = document.getElementById('xiaoxiangClick');
-  xiaoxiang.style.opacity = 1;
-  btn.style.opacity = 0;
+yueer.addEventListener('touchend', function() {
+        const btn = document.getElementById('yueerClick');
+        yueer.style.opacity = 1;
+        btn.style.opacity = 0;
+    })
+    // 潇湘
+xiaoxiang.addEventListener('touchstart', function() {
+    const btn = document.getElementById('xiaoxiangClick');
+    xiaoxiang.style.opacity = 0;
+    btn.style.opacity = 1;
 })
 
+xiaoxiang.addEventListener('touchend', function() {
+    const btn = document.getElementById('xiaoxiangClick');
+    xiaoxiang.style.opacity = 1;
+    btn.style.opacity = 0;
+})
