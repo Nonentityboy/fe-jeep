71bf4c0190a45aa44ddd60c2329e8824b6cb4956
Nonentityboy <1138580559@qq.com>
2020-12-04 01:35:04 +0800

    update

--- js/moveMoon.js
+++ js/moveMoon.js
@@ -8,6 +8,16 @@ var startY = 0;
 var y = 0;
 var tint;
 let lastTop;
+
+
+const headerTextDiv = document.getElementById('header-bottom-text');
+const middleTextDiv = document.getElementById('middle-text-tips');
+const hands = document.getElementById('hands');
+
+const maxTop1 = headerTextDiv.offsetTop;
+const maxTop2 = middleTextDiv.offsetTop;
+const maxTop3 = hands.offsetTop;
+
 // 手指触摸 
 div.addEventListener('touchstart', function(e) {
     clearInterval(tint);
@@ -23,28 +33,25 @@ div.addEventListener('touchend', function(e) {
     const body = document.querySelector('body');
     const moonDiv = document.getElementById('footer-bottom-middle');
 
-    const headerTextDiv = document.getElementById('header-bottom-text');
-    const middleTextDiv = document.getElementById('middle-text-tips');
-
-    // 控制那一块
-    const middleTextTipsDiv = document.getElementById('middle-text-tips')
-
     const maxTop = body.offsetHeight - moonDiv.offsetHeight;
     console.log(maxTop, lastTop)
     if (lastTop <= maxTop && lastTop >= maxTop / 2) {
         tint = setInterval(() => {
+                // headerTextDiv.style.top = maxTop + 'px';
                 moonDiv.style.top = maxTop + 'px';
+                headerTextDiv.style.top = maxTop1 + 'px';
+                middleTextDiv.style.top = maxTop2 + 'px';
+                hands.style.top = maxTop3 + 'px';
                 clearInterval(tint);
             },
             200);
         moonDiv.style.transition = "ease 2s";
-    } else {
-        // 控制第一幕第一组 字体消失。
-        headerTextDiv.style.opacity = 0;
-        headerTextDiv.style.transition = "ease 2s";
-        middleTextDiv.style.opacity = 0;
-        middleTextDiv.style.transition = "ease 2s";
 
+
+        headerTextDiv.style.transition = "ease 2s";
+        middleTextDiv.style.transition = "ease 2s";
+        hands.style.transition = "ease 2s";
+    } else {
         // 控制第一幕第二组 孔明灯出现
         moveStartGroup()
 
@@ -52,6 +59,10 @@ div.addEventListener('touchend', function(e) {
         moonDiv.style.top = 0;
         moonDiv.style.opacity = 0;
         moonDiv.style.transition = "ease 2s";
+        middleTextDiv.style.display = 'none';
+        middleTextDiv.style.transition = "ease 2s";
+        hands.style.display = 'none';
+        hands.style.transition = "ease 2s";
     }
     moonDiv.style.boxShadow = '';
 });
@@ -64,11 +75,16 @@ div.addEventListener('touchmove', function(e) {
     // 计算手指的移动距离：手指移动之后的坐标减去手指初始的坐标 
     let moveY = e.targetTouches[0].pageY - startY;
     lastTop = y + moveY;
+
     //防止超出父元素范围
     if (lastTop <= 0) lastTop = 0;
     if (lastTop >= maxTop) lastTop = maxTop;
     // 移动盒子 盒子原来的位置 + 手指移动的距离 
     this.style.top = lastTop + 'px';
+
+    headerTextDiv.style.top = maxTop1 + moveY + 'px';
+    middleTextDiv.style.top = maxTop2 + moveY + 'px';
+    hands.style.top = maxTop3 + moveY + 'px';
     // 控制台 打印位置
     // console.log('顶部最大高度' + maxTop)
     // console.log('距离顶部高度' + lastTop + 'px');
