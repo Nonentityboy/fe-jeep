1951244d8465800b13d76d53b37f68f1b65ec100
jiangjie1105 <1021404964@qq.com>
2020-12-08 22:47:38 +0800

    2247

--- js/yueshiClick.js
+++ js/yueshiClick.js
@@ -3,21 +3,97 @@
 // https://s3.ax1x.com/2020/12/08/rSxmlT.png       阮籍
 // https://s3.ax1x.com/2020/12/08/rSxepV.png       师旷
 // https://s3.ax1x.com/2020/12/08/rSxn6U.png       伯牙
-// https://s3.ax1x.com/2020/12/08/rSxl79.png       菜k。。
+// https://s3.ax1x.com/2020/12/08/rSxl79.png       菜yong
 // https://s3.ax1x.com/2020/12/08/rSxQ0J.png        董廷兰
 // https://s3.ax1x.com/2020/12/08/rSx8t1.png       郭楚忘
 
-
+// 获取图片节点
 const textjikangimg = document.getElementById('jikangimg');
+const textruanjiimg = document.getElementById('ruanjiimg');
+const textboyaimg = document.getElementById('boyaimg');
+const textkongziimg = document.getElementById('kongziimg');
+const textcaiyongimg = document.getElementById('caiyongimg');
+const textdongtinglanimg = document.getElementById('dongtinglanimg');
+const textguochuwangimg = document.getElementById('guochuwangimg');
+const textshikuangimg = document.getElementById('shikuangimg');
 
 
-
-textjikangimg.addEventListener('touchstart', function() {
+// 嵇康铜盘
+textjikangimg.addEventListener('touchstart', function () {
     const btn = document.getElementById('jikangimgtongpan');
     btn.style.opacity = 1;
 })
 
-textjikangimg.addEventListener('touchend', function() {
+textjikangimg.addEventListener('touchend', function () {
     const btn = document.getElementById('jikangimgtongpan');
+    btn.style.opacity = 0;
+})
+// 阮籍铜盘
+textruanjiimg.addEventListener('touchstart', function () {
+    const btn1 = document.getElementById('ruanjiimgtongpan');
+    btn1.style.opacity = 1;
+})
+textruanjiimg.addEventListener('touchend', function () {
+    const btn1 = document.getElementById('ruanjiimgtongpan');
+    btn1.style.opacity = 0;
+})
+// 伯牙铜盘
+textboyaimg.addEventListener('touchstart', function () {
+    const btn = document.getElementById('boyaimgtongpan');
+    btn.style.opacity = 1;
+})
+
+textboyaimg.addEventListener('touchend', function () {
+    const btn = document.getElementById('boyaimgtongpan');
+    btn.style.opacity = 0;
+})
+// 孔子铜盘
+textkongziimg.addEventListener('touchstart', function () {
+    const btn = document.getElementById('kongziimgtongpan');
+    btn.style.opacity = 1;
+})
+
+textkongziimg.addEventListener('touchend', function () {
+    const btn = document.getElementById('kongziimgtongpan');
+    btn.style.opacity = 0;
+})
+// 蔡邕铜盘
+textcaiyongimg.addEventListener('touchstart', function () {
+    const btn = document.getElementById('caiyongimgtongpan');
+    btn.style.opacity = 1;
+})
+
+textcaiyongimg.addEventListener('touchend', function () {
+    const btn = document.getElementById('caiyongimgtongpan');
+    btn.style.opacity = 0;
+})
+// 董廷兰铜盘
+textdongtinglanimg.addEventListener('touchstart', function () {
+    const btn = document.getElementById('dongtinglanimgtongpan');
+    btn.style.opacity = 1;
+})
+
+textdongtinglanimg.addEventListener('touchend', function () {
+    const btn = document.getElementById('dongtinglanimgtongpan');
+    btn.style.opacity = 0;
+})
+// 郭楚忘铜盘
+textguochuwangimg.addEventListener('touchstart', function () {
+    const btn = document.getElementById('guochuwangimgtongpan');
+    btn.style.opacity = 1;
+})
+
+textguochuwangimg.addEventListener('touchend', function () {
+    const btn = document.getElementById('guochuwangimgtongpan');
+    btn.style.opacity = 0;
+})
+// 师旷铜盘
+textshikuangimg.addEventListener('touchstart', function () {
+    const btn = document.getElementById('shikuangimgtongpan');
+    btn.style.opacity = 1;
+})
+
+textshikuangimg.addEventListener('touchend', function () {
+    const btn = document.getElementById('shikuangimgtongpan');
     btn.style.opacity = 0;
 })
