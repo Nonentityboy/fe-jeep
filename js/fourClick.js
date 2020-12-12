a51190e03156583f114f95dc22e38708fa440a53
Nonentityboy <1138580559@qq.com>
2020-12-12 13:51:16 +0800

    update

---
+++ js/fourClick.js
@@ -0,0 +1,13 @@
+const headerBottomHandsDiv = document.getElementById('header-bottom-text-group-three-hands');
+
+const threeGroupInfoDiv = document.getElementById('threeGroup');
+
+headerBottomHandsDiv.addEventListener('touchend', function(e) {
+    threeGroupInfoDiv.style.display = "none";
+    toolDiv.style.background = "url('https://s3.ax1x.com/2020/12/12/rV3JFP.png') no-repeat #efeff4";
+    toolDiv.style.backgroundSize = "100%";
+    toolDiv.style.display = "block"
+    toolDiv.style.width = "100%";
+    toolDiv.style.height = "7260px";
+    e.preventDefault();
+})
