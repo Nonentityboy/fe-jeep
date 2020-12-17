df44f94e2bf2da2082fa561c9430ad9fbf079848
Nonentityboy <1138580559@qq.com>
2020-12-17 20:34:21 +0800

    update

--- js/stoptouch2.js
+++ js/stoptouch2.js
@@ -1,11 +1,38 @@
 const container_four = document.getElementById('container_four');
 
-container_four.addEventListener('touchstart', function (e) {
-  e.preventDefault();
+container_four.addEventListener('touchstart', function(e) {
+    e.preventDefault();
 });
-container_four.addEventListener('touchend', function (e) {
-  e.preventDefault();
+container_four.addEventListener('touchend', function(e) {
+    e.preventDefault();
 });
-container_four.addEventListener('touchmove', function (e) {
-  e.preventDefault();
+container_four.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+});
+
+const cut_hands_move = document.getElementById('cut_hands_move')
+
+cut_hands_move.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+});
+
+cut_hands_move.addEventListener('touchstart', function(e) {
+    e.preventDefault();
+});
+cut_hands_move.addEventListener('touchend', function(e) {
+    e.preventDefault();
+});
+
+
+const hhhhhhhhh = document.getElementById('header-bottom-text-group-three-bottom');
+
+hhhhhhhhh.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+});
+
+hhhhhhhhh.addEventListener('touchstart', function(e) {
+    e.preventDefault();
+});
+hhhhhhhhh.addEventListener('touchend', function(e) {
+    e.preventDefault();
 });
