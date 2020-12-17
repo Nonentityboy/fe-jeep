6c20207ee8f2c8750e5d5549c0fb22f84840130a
jiangjie1105 <1021404964@qq.com>
2020-12-17 18:44:41 +0800

    1844

--- js/stoptouch.js
+++ js/stoptouch.js
@@ -1,40 +1,40 @@
-middleTextDiv.addEventListener('touchstart', function(e) {
+middleTextDiv.addEventListener('touchstart', function (e) {
     e.preventDefault();
 });
 
-middleTextDiv.addEventListener('touchend', function(e) {
+middleTextDiv.addEventListener('touchend', function (e) {
     e.preventDefault();
 });
 
-middleTextDiv.addEventListener('touchmove', function(e) {
+middleTextDiv.addEventListener('touchmove', function (e) {
     e.preventDefault();
 });
 
 const kmLine = document.getElementById('km-line');
 
-kmLine.addEventListener('touchmove', function(e) {
+kmLine.addEventListener('touchmove', function (e) {
     e.preventDefault();
 });
 
-kmLine.addEventListener('touchend', function(e) {
+kmLine.addEventListener('touchend', function (e) {
     e.preventDefault();
 });
 
-kmLine.addEventListener('touchstart', function(e) {
+kmLine.addEventListener('touchstart', function (e) {
     e.preventDefault();
 });
 
 const nolineDesc = document.getElementById('noneline-descript');
 
-nolineDesc.addEventListener('touchstart', function(e) {
+nolineDesc.addEventListener('touchstart', function (e) {
     e.preventDefault();
 });
 
-nolineDesc.addEventListener('touchend', function(e) {
+nolineDesc.addEventListener('touchend', function (e) {
     e.preventDefault();
 });
 
-nolineDesc.addEventListener('touchmove', function(e) {
+nolineDesc.addEventListener('touchmove', function (e) {
     e.preventDefault();
 });
 
@@ -47,112 +47,114 @@ const threeHalfGroup4 = document.getElementById('threeHalfGroup4');
 const threeHalfGroup5 = document.getElementById('threeHalfGroup5');
 const threeHalfGroup6 = document.getElementById('threeHalfGroup6');
 
-threeHalfGroup1.addEventListener('touchstart', function(e) {
+threeHalfGroup1.addEventListener('touchstart', function (e) {
     e.preventDefault();
 });
 
 
-threeHalfGroup1.addEventListener('touchend', function(e) {
+threeHalfGroup1.addEventListener('touchend', function (e) {
     e.preventDefault();
 });
 
-threeHalfGroup1.addEventListener('touchmove', function(e) {
-    e.preventDefault();
-});
-
-
-threeHalfGroup2.addEventListener('touchstart', function(e) {
+threeHalfGroup1.addEventListener('touchmove', function (e) {
     e.preventDefault();
 });
 
 
-threeHalfGroup2.addEventListener('touchend', function(e) {
-    e.preventDefault();
-});
-
-threeHalfGroup2.addEventListener('touchmove', function(e) {
+threeHalfGroup2.addEventListener('touchstart', function (e) {
     e.preventDefault();
 });
 
 
-threeHalfGroup3.addEventListener('touchstart', function(e) {
+threeHalfGroup2.addEventListener('touchend', function (e) {
+    e.preventDefault();
+});
+
+threeHalfGroup2.addEventListener('touchmove', function (e) {
     e.preventDefault();
 });
 
 
-threeHalfGroup3.addEventListener('touchend', function(e) {
-    e.preventDefault();
-});
-
-threeHalfGroup3.addEventListener('touchmove', function(e) {
-    e.preventDefault();
-});
-
-threeHalfGroup4.addEventListener('touchstart', function(e) {
+threeHalfGroup3.addEventListener('touchstart', function (e) {
     e.preventDefault();
 });
 
 
-threeHalfGroup4.addEventListener('touchend', function(e) {
+threeHalfGroup3.addEventListener('touchend', function (e) {
     e.preventDefault();
 });
 
-threeHalfGroup4.addEventListener('touchmove', function(e) {
+threeHalfGroup3.addEventListener('touchmove', function (e) {
     e.preventDefault();
 });
 
-threeHalfGroup5.addEventListener('touchstart', function(e) {
-    e.preventDefault();
-});
-
-
-threeHalfGroup5.addEventListener('touchend', function(e) {
-    e.preventDefault();
-});
-
-threeHalfGroup5.addEventListener('touchmove', function(e) {
+threeHalfGroup4.addEventListener('touchstart', function (e) {
     e.preventDefault();
 });
 
 
-threeHalfGroup6.addEventListener('touchstart', function(e) {
+threeHalfGroup4.addEventListener('touchend', function (e) {
+    e.preventDefault();
+});
+
+threeHalfGroup4.addEventListener('touchmove', function (e) {
+    e.preventDefault();
+});
+
+threeHalfGroup5.addEventListener('touchstart', function (e) {
     e.preventDefault();
 });
 
 
-threeHalfGroup6.addEventListener('touchend', function(e) {
+threeHalfGroup5.addEventListener('touchend', function (e) {
     e.preventDefault();
 });
 
-threeHalfGroup6.addEventListener('touchmove', function(e) {
+threeHalfGroup5.addEventListener('touchmove', function (e) {
+    e.preventDefault();
+});
+
+
+threeHalfGroup6.addEventListener('touchstart', function (e) {
+    e.preventDefault();
+});
+
+
+threeHalfGroup6.addEventListener('touchend', function (e) {
+    e.preventDefault();
+});
+
+threeHalfGroup6.addEventListener('touchmove', function (e) {
     e.preventDefault();
 });
 
 const beijixingStopTouch = document.getElementById('eader-bottom-text-group-three');
 
-beijixingStopTouch.addEventListener('touchstart', function(e) {
+beijixingStopTouch.addEventListener('touchstart', function (e) {
     e.preventDefault();
 });
 
-beijixingStopTouch.addEventListener('touchend', function(e) {
+beijixingStopTouch.addEventListener('touchend', function (e) {
     e.preventDefault();
 });
 
-beijixingStopTouch.addEventListener('touchmove', function(e) {
+beijixingStopTouch.addEventListener('touchmove', function (e) {
     e.preventDefault();
 });
+
 
 const yueqiqqqqqq = document.getElementById('eader-bottom-text-group-three');
 
 
-yueqiqqqqqq.addEventListener('touchstart', function(e) {
+yueqiqqqqqq.addEventListener('touchstart', function (e) {
     e.preventDefault();
 });
 
-yueqiqqqqqq.addEventListener('touchend', function(e) {
+yueqiqqqqqq.addEventListener('touchend', function (e) {
     e.preventDefault();
 });
 
-yueqiqqqqqq.addEventListener('touchmove', function(e) {
+yueqiqqqqqq.addEventListener('touchmove', function (e) {
     e.preventDefault();
 });
+
---
+++ js/stoptouch2.js
@@ -0,0 +1,11 @@
+const container_four = document.getElementById('container_four');
+
+container_four.addEventListener('touchstart', function (e) {
+  e.preventDefault();
+});
+container_four.addEventListener('touchend', function (e) {
+  e.preventDefault();
+});
+container_four.addEventListener('touchmove', function (e) {
+  e.preventDefault();
+});
